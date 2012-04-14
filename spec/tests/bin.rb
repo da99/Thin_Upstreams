b1cd52a07a1508ddd54328dfe8f981532f4764f5
da99 <i-hate-spam-45671204@mailinator.com>
2012-04-14 07:31:43 -0500

    Finished bin executable implementation.

--- spec/tests/bin.rb
+++ spec/tests/bin.rb
@@ -1,13 +1,34 @@
 
 bins = Dir.glob("bin/*")
 
-unless bins.empty?
-  describe "permissions of bin/" do
-    bins.each { |file|
-      it "should chmod 755 for: #{file}" do
-        `stat -c %a #{file}`.strip
-        .should.be == "755"
-      end
+describe "permissions of bin/" do
+  bins.each { |file|
+    it "should chmod 755 for: #{file}" do
+      `stat -c %a #{file}`.strip
+      .should.be == "755"
+    end
+  }
+end # === permissions of bin/
+
+describe "Thin_Upstreams (bin executable)" do
+
+  behaves_like "file_maker"
+
+  it "creates upstreams.conf file" do
+    target = "upstream Hi"
+    chdir {
+      Exit_Zero "Thin_Upstreams"
+      File.read("upstreams.conf")[target].should == target
     }
-  end # === permissions of bin/
-end # === unless bins.empty?
+  end
+  
+  it "accepts a file glob" do
+    target = ":601"
+    chdir {
+      Exit_Zero "Thin_Upstreams \"*/custom/*.yml\""
+      File.read("upstreams.conf")[target].should == target
+    }
+  end
+  
+end # === Thin_Upstreams (bin executable)
+
